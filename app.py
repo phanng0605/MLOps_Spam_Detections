8bcf518925f8268770dbc419f8ec251f76ceb328
Phan Nguyen <94454067+phanng0605@users.noreply.github.com>
2023-10-06 15:55:45 +0000

    Created email spam classifier

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 from utils import model_predict
 
@@ -17,6 +17,14 @@ def predict():
     email = request.form.get('content')
     prediction = model_predict(email)
     return render_template("index.htm", prediction=prediction, email=email)
+
+
+@app.route("/api/predict", methods=["POST"])
+def api_predict():
+    data = request.get_json(force=True)
+    email = data['content']
+    prediction = model_predict(email=email)
+    return jsonify({'prediction': prediction, 'email':email})
 
 
 if __name__ == "__main__":
